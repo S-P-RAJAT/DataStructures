6763bc41e404749a591baa4147504e108096126e
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-04 00:42:22 +0530

    MODIFIED: Searched keyword, if found is deleted else inserted into list

--- src/com/bridgelabz/datastructures/UnOrderedList.java
+++ src/com/bridgelabz/datastructures/UnOrderedList.java
@@ -20,7 +20,18 @@ public class UnOrderedList {
 		String wordToBeSearched = scanner.next();
 		System.out.println(wordToBeSearched);
 		INodeIF<String> newNode = linkedListObject.search(wordToBeSearched);
-		
+		if (newNode == null) {
+			MyNodeImpl<String> node = new MyNodeImpl<String>(wordToBeSearched);
+			linkedListObject.appendNode(node);
+			System.out.println(wordToBeSearched + " not found!");
+
+		} else {
+			System.out.println(wordToBeSearched + " is found!");
+
+			linkedListObject.deleteGivenKeyNode(wordToBeSearched);
+			System.out.println("The word is deleted");
+		}
+		linkedListObject.printNodes();
 		scanner.close();
 	}
 }
